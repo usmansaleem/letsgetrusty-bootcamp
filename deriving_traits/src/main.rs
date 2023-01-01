0f1b7a66c7b0c33dba59a33b76ac12de6802c6aa
Usman Saleem <usman@usmans.info>
2023-01-01 10:25:28 +1000

    Deriving Traits examples

---
+++ deriving_traits/src/main.rs
@@ -0,0 +1,16 @@
+#[derive(Debug, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+fn main() {
+    println!("Hello, world!");
+    let p1 = Point { x: 3, y: 1 };
+    let p2 = Point { x: 3, y: 1 };
+    let p3 = Point { x: 5, y: 5 };
+
+    println!("{:?}", p1);
+    println!("{}", p1 == p2);
+    println!("{}", p1 == p3);
+}
